dd8f8f546a254a7b84d7e78be5b5b67bd53c5e76
Robby Dore <rdore88@gmail.com>
2017-09-25 22:04:12 -0400

    Created FilterTodo container

---
+++ src/containers/FilterTodo.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {filterTodos} from '../actions/index';
+
+class FilterTodos extends Component{
+  createFilterRadio(value, text){
+    return(
+      <label key={value}>
+        <input type="radio" name="showTodos" value={value} checked={this.propsfilter == value} onChange={event => this.props.filterTodos(value)} /> {text}
+        </label>
+    )
+  }
+
+  render(){
+    const filterOptions = [
+      [
+        "all", "Show all"
+      ],
+      [
+        "uncompleted", "Show uncompleted"
+      ],
+      [
+        "completed", "Show completed"
+      ]
+    ]
+
+    return(
+      <div className="FilterTodos">
+        {filterOptions.map(option => this.createFilterRadio.apply(this, option))}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = function(state){
+  return{filter: state.filter}
+}
+
+const mapDispatchToProps = function(dispatch){
+  return{
+    filterTodos: function(filter){
+      dispatch(filterTodos(filter));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterTodos)
