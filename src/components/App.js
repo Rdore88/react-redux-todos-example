d6b3ac59ef42285f6fc087a8d6150ad7d3fc15cb
Robby Dore <rdore88@gmail.com>
2017-09-25 22:12:37 -0400

    Refactored App.js to create store and provider component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import createTodo from '../containers/CreateTodo';
+import FilterTodos from '../containers/FilterTodos';
+import TodoList from '../containers/TodoList';
+
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import todoApp from '../reducers/todo';
+
+const store = createStore(todoApp);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Provider store={store}>
+        <div className="App">
+          <CreateTodo />
+          <FilterTodos />
+          <TodoList />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Provider>
     );
   }
 }
